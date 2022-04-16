3b9db79c3a024de5b34986a0ccf9996ebfc1c7a0
Gabriel Lima <gabrielpk1@live.com>
2022-04-16 14:18:24 -0300

    add docker-compose file

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
+import cors from "cors";
 import express from "express";
 
 import { router } from "./routes";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use(express.json());
 
